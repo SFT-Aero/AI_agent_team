8677e88e8e76755cc6fbc44e4edfac46e576f555
Shandler Mason <samason4@ncsu.edu>
2025-06-17 09:38:21 -0400

    defined functionality of data ingestor agent

--- agents/beginning/data_ingester_agent.py
+++ agents/beginning/data_ingester_agent.py
@@ -1,3 +1,9 @@
-# receives and preps webscrapper output
+# Receives and prepares the webscrappers output to be interpreted by agent team
+# Makes sure that all downstream agents receive clean, consistent data regardless of source
+# This agent will:
+    # Standardize categories: ("Tech" → "Technology")
+    # Fix dates: parse strings into datetime.date objects or standardized strings
+    # Fallbacks: fill missing fields with defaults ('No teaser found')
+    # Clean HTML: remove tags from body/teaser fields
 
 from crewai import Agent
