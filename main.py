5c7246c1ab98ee649062685d9a519c96c7ccf231
Shandler Mason <samason4@ncsu.edu>
2025-07-01 10:22:11 -0400

    added comment to main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from scrapper.webscrapper import scrap
 
 from futuristic_agent import futuristic_task, futuristic_agent
 
-# Step 1: Clean raw scraped data
-raw_data = scrap() # Run your webscraper and get article data
+# Step 1: Runs the web scraper and gets the article data
+raw_data = scrap()
 print(f"Raw Data: {len(raw_data)} articles")
 
-#Step 2: Futuristic agent
+#Step 2: Pass article data to futuristic agent
 futuristic_task = futuristic_task(raw_data)
 
-# Step 3: Run the crew
+# Step 3: Runs the agent crew
 crew = Crew(
     agents=[futuristic_agent],
     tasks=[futuristic_task]
