6510925f04a76e33da9d86031dff41fa7594ac27
Shandler Mason <samason4@ncsu.edu>
2025-08-06 00:14:20 -0400

    corrected home paths

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from crewai import Crew, Process
 from scrapper.webscrapper import scrap, read_urls_from_csv, write_scraped_data_to_csv
 from futuristic_agent import futuristic_task, futuristic_agent, disruptive_economy_task, parse_agent_output, save_to_csv
+from pathlib import Path
 
-#Step 1: Set up GUI
-input_csv = '/Users/shan/AI_agent_team/urls.csv'  # Path to your input CSV with URLs
-output_csv = '/Users/shan/AI_agent_team/scraped_articles.csv'  # Path for the output CSV
-signals_csv = '/Users/shan/AI_agent_team/general_signals.csv'
-econ_csv = '/Users/shan/AI_agent_team/economic_signals.csv'
+home = Path.home()  # This is /Users/yourname
+#path = home / 'AI_agent_team' / 'scrapper' / 'ws_output.html'
+
+input_csv = home / 'AI_agent_team' / 'urls.csv'
+output_csv = home / 'AI_agent_team' / 'scraped_articles.csv'
+signals_csv = home / 'AI_agent_team' / 'general_signals.csv'
+econ_csv = home / 'AI_agent_team' / 'economic_signals.csv'
 
 # Step 1: Read URLs from the input CSV
 urls = read_urls_from_csv(input_csv)
